a38104c519647bc8126b2194dea8602fc490af01
lvaughan <luke.vaughan@okstate.edu>
2025-12-31 13:10:00 -0600

    clean up

--- main.py
+++ main.py
@@ -6,5 +6,5 @@ dataset_path="./datasets/R0_DATA_FLEX_F1/R0_Triplet_Data_Flex_F1_F_White_bg/"
 preprocess_path="./preprocess/"
 
 do_preprocessing(dataset_path, preprocess_path)
-model = ConvAutoencoder(embed_dim=64, batch_size=4)
+model = ConvAutoencoder(embed_dim=64)
 do_training(preprocess_path+"dataset.npy", model)
