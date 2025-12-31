6e2b6d93afd42e9d2b51a900fd8c25673ca728db
lvaughan <luke.vaughan@okstate.edu>
2025-12-31 11:42:25 -0600

    setup conda env

--- setup.sh
+++ setup.sh
@@ -1,26 +1,5 @@
 #!/bin/bash
-
-# Store the current directory
-current_dir=$(pwd)
-
-# Change to the venv directory
-cd venv
-
-# Check if environment already exists
-if [ ! -f ./venv/bin/activate ]; then
-    # Create venv using python module
-    python3 -m venv venv
-
-    # Activate the virtual environment
-    source venv/bin/activate
-
-    # Install requirements
-    pip install -r requirements.txt
-
-else
-    # Activate the virtual environment
-    source venv/bin/activate
-fi
-
-# Return to the original directory
-cd "$current_dir"
+# List conda envs, search for existing install, if not found then create, else continue
+conda env list | grep -q "^vis-inspec " || conda env create -f conda/environment.yaml
+# Activate the environment
+conda activate vis-inspec
