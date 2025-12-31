a18036c31e2908ba9cb3196323a0c32b4c14c2d1
lvaughan <luke.vaughan@okstate.edu>
2025-12-31 12:27:43 -0600

    include training

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
 from preprocess.preprocessing import do_preprocessing
+from train.model import ConvAutoencoder
+from train.training import do_training
 
-do_preprocessing()
+dataset_path="./datasets/R0_DATA_FLEX_F1/R0_Triplet_Data_Flex_F1_F_White_bg/"
+preprocess_path="./preprocess/"
+
+do_preprocessing(dataset_path, preprocess_path)
+model = ConvAutoencoder(embed_dim=64, batch_size=4)
+do_training(preprocess_path+"dataset.npy", model)
