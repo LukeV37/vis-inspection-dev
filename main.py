5976ebaa356f044742ae0d5d9ef960863180531b
Geeck03 <aramaea.marie@gmail.com>
2026-01-26 20:54:15 -0600

    added call to eval in main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from preprocess.preprocessing import do_preprocessing
 from train.model import ConvAutoencoder
 from train.training import do_training
+from train.eval import eval_model 
 
 dataset_path="./datasets/R0_DATA_FLEX_F1/R0_Triplet_Data_Flex_F1_F_White_bg/"
 preprocess_path="./preprocess/"
@@ -8,3 +9,4 @@ preprocess_path="./preprocess/"
 do_preprocessing(dataset_path, preprocess_path)
 model = ConvAutoencoder(embed_dim=64)
 do_training(preprocess_path+"dataset.npy", model)
+eval_model("../preprocess/dataset.npy", out_dir="predictions")
