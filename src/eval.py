a9101661854c8d5bb33552ac71515f0d792ff3f8
lvaughan <luke.vaughan@okstate.edu>
2026-01-31 18:54:12 -0600

    move dataset creation to main

--- src/eval.py
+++ src/eval.py
@@ -5,21 +5,19 @@ from tqdm import tqdm
 import matplotlib.pyplot as plt
 import tensorflow as tf
 
-def eval_model(in_path, in_model, out_dir, model, batch_size):
+def eval_model(test_dataset, path, out_dir, batch_size):
     # Create the output directory
     os.makedirs(out_dir, exist_ok=True)
 
-    # Load the dataset
-    test_file_paths = sorted(glob.glob(in_path+'Test/preprocessed*.npy'))
-    test_dataset = create_dataset(test_file_paths, batch_size=batch_size, shuffle=False)
-
+    # Get tag from file names
+    test_file_paths = sorted(glob.glob(path+'Test/preprocessed*.npy'))
     names = [os.path.basename(x) for x in test_file_paths]
     tag = [name[12:-4] for name in names]
 
     # load the trained weights
     dummy_input = tf.zeros((1, 1080, 1920, 3))
     _ = model(dummy_input)
-    model.load_weights(in_model)
+    model.load_weights(path+"model.weights.h5")
 
     # run the predictions
     pred_image = model.predict(test_dataset)
@@ -35,9 +33,15 @@ def eval_model(in_path, in_model, out_dir, model, batch_size):
 if __name__ == "__main__":
     from model import ConvAutoencoder
     from data_loader import create_dataset
+
+    # Parameters
     model = ConvAutoencoder(embed_dim=64, channels=16)
-    in_data = "../output_debug/"
-    in_model = "../output_debug/model.weights.h5"
+    path = "../output_debug/"
     out_dir = "../output_debug/Predictions"
     batch_size = 32
-    eval_model(in_data, in_model, out_dir, model, batch_size)
+
+    # Load the dataset
+    test_file_paths = sorted(glob.glob(path+'Test/preprocessed*.npy'))
+    test_dataset = create_dataset(test_file_paths, batch_size=batch_size, shuffle=False)
+
+    eval_model(test_dataset, path, out_dir, batch_size)
